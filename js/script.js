b0be8fdb8c552ad5d467c2e1ec9f300095759b90
Samuel Estevez <julioestevez@aclanyc.com>
2019-01-03 23:22:09 +0000

    aaaaa

--- js/script.js
+++ js/script.js
@@ -1,33 +1,43 @@
 /* global $ */
-var melee = 0;
-var brawl = 0;
-var smash4 = 0;
-var ultimate = 0;
+var totalScore = 0;
+
 $("button").click(function() {
     var name = $("#name").val();
-    var techPlacement = "TBD";
+    var placement = "TBD";
     var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
     var q1Result = $("#question1").val().toLowerCase();
     var q2Result = $("#question2").val().toLowerCase();
     var q3Result = $("#question3").val().toLowerCase();
     
     if(q1Result === "yes"){
-        melee++;
-        ultimate++;
-    }else{
-        brawl++;
-        smash4++;
+        totalScore++;
+    }
+    if(q2Result === "yes"){
+        totalScore++;
+    }
+    if(q3Result === "yes"){
+        totalScore++;
     }
     
-    if(q2Result === ""){
-        
-    }else{
-        
+    function result(){
+        if(q1Result === "no" && q2Result === "no" && q3Result === "no"){
+            return "Brawl";
+        }else if(q1Result === "yes" && q2Result === "no" && q3Result === "no"){
+            return "Ultimate";
+        }else if(q1Result === "no" && q2Result === "yes" && q3Result === "no"){
+            return "N64";
+        }else if(q1Result === "no" && q2Result === "no" && q3Result === "yes"){
+            return "Smash 4";
+        }else if(q1Result === "yes" && q2Result === "yes" && q3Result === "no"){
+            return "Melee";
+        }else if(q1Result === "no" && q2Result === "yes" && q3Result === "yes"){
+            return "Brawl";
+        }else if(q1Result === "yes" && q2Result === "no" && q3Result === "yes"){
+            return "Ultimate";
+        }else if(q1Result === "yes" && q2Result === "yes" && q3Result === "yes"){
+            return "Melee";
+        }
     }
-    
-    if(q3Result === ""){
-        
-    }else{
-        
-    }
+
+    $("#result").append(result());
 });
